8f97e87246a44f4628454833307b21cacc1ede3a
Jose <dudujose.009@gmail.com>
2025-03-09 12:51:01 -0300

    atualizacao, inserido cadastro de compras

--- Minhascompras/App.xaml.cs
+++ Minhascompras/App.xaml.cs
@@ -1,7 +1,28 @@
-﻿namespace Minhascompras
+﻿using Minhascompras.Helpers;
+
+namespace Minhascompras
 {
     public partial class App : Application
     {
+        static SQLiteDataBaseHelpers _db;
+
+        public static SQLiteDataBaseHelpers Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDataBaseHelpers(path);
+                }
+                return _db;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
--- Minhascompras/Views/Novoproduto.xaml.cs
+++ Minhascompras/Views/Novoproduto.xaml.cs
@@ -1,3 +1,5 @@
+using Minhascompras.Models;
+
 namespace Minhascompras.Views;
 
 public partial class Novoproduto : ContentPage
@@ -6,4 +8,25 @@ public partial class Novoproduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto
+			{
+				Descricao = txt_preco.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+
+			await App.Db.Insert(p);
+			await DisplayAlert("Sucesso!", "Registrado Inserido", "OK");
+
+		}
+		catch (Exception ex) 
+		{
+			await DisplayAlert("Ops", ex.Message, "Ok");
+		}
+    }
 }
